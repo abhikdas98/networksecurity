e4d3076fc70f8e4a7577fc65026d011e09a7e422
Abhik Das <abhikdss98123@gmail.com>
2026-04-29 12:17:38 +0530

    Data validation completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -10,7 +10,7 @@ class NetworkSecurityException(Exception):
         self.file_name = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
-        return "Erro occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name, self.lineno, self.error_message)
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name, self.lineno, self.error_message)
 
 if __name__ == "__main__":
     try:
